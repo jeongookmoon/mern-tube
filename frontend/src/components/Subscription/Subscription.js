6e609249b21eb8465917a6aef489e26edf55dbc6
Jeong Ook Moon <moonjeongook@gmail.com>
2020-09-10 23:30:06 -0700

    initiate subscription page

---
+++ frontend/src/components/Subscription/Subscription.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from 'antd';
+import axios from 'axios';
+import Videos from './Items/Videos';
+
+const Subscription = () => {
+  return (
+    <div>
+      Subscription
+    </div>
+  );
+};
+
+export default Subscription;
